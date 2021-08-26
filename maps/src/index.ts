34511be106a71a879d0c28965aac2d43d60fc01c
Gabriel Arellano <anewlevelmp@gmail.com>
2021-08-25 19:46:48 -0700

    feat(main): add console.logs back in;

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -23,9 +23,9 @@ const company: Company = new Company();
 const map: Map = new Map('map');
 const user: User = new User();
 
-// console.log(company);
-// console.log(map);
-// console.log(user);
+console.log(company);
+console.log(map);
+console.log(user);
 
 map.addMarker(company);
 map.addMarker(user);
