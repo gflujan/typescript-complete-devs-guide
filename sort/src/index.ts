5f3475d1b806b7c7b2b4a956ad02dbe325752fdd
Gabriel F. Lujan <gflujan@gmail.com>
2021-09-14 21:06:37 -0700

    feat(main): test logic by updating array of nums;

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -2,7 +2,7 @@
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
-const numbersCollection: NumbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const numbersCollection: NumbersCollection = new NumbersCollection([10, 300, -5, 0]);
 const sorter: Sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
