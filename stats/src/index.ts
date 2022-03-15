7ca5612a2893d91fbd7b6ae9e6e93b1dc624960b
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-03-14 21:07:49 -0700

    feat(main): refactor `index.ts` to use better class types when creating data readers;

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -5,6 +5,7 @@
 // Packages
 // Context / Store / Router
 // Components / Classes / Controllers / Services
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 // Assets
 // Constants / Models / Interfaces / Types
@@ -12,12 +13,16 @@ import { MatchResult } from './MatchResult';
 // Utils / Methods / Mocks
 // Styles
 
-const reader: MatchReader = new MatchReader('football-data.csv');
-reader.read();
+// Step 1: Create an object that satisfies the `DataReader` interface
+const csvFileReader: CsvFileReader = new CsvFileReader('football-data.csv');
+
+// Step 2: Create an instance of `MatchReader` and pass in something satisfying the `DataReader` interface
+const matchReader: MatchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
       manUnitedWins += 1;
    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
