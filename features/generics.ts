4259bda5fde300e3648ab294b666035fbf75c2b0
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-03-28 20:25:15 -0700

    feat(main): add `generics` example classes;

--- features/generics.ts
+++ features/generics.ts
@@ -23,3 +23,65 @@ class ArrayOfAnything<T> {
 }
 
 new ArrayOfAnything<string>(['a', 'b', 'c']);
+
+/* ========================================================================== */
+// Example of generics w/ functions
+/* ========================================================================== */
+function printStrings(arr: Array<string>): void {
+   for (let i = 0; i < arr.length; i += 1) {
+      console.log(arr[i]);
+   }
+}
+
+function printNumbers(arr: Array<number>): void {
+   for (let i = 0; i < arr.length; i += 1) {
+      console.log(arr[i]);
+   }
+}
+
+function printAnything<T>(arr: Array<T>): void {
+   for (let i = 0; i < arr.length; i += 1) {
+      console.log(arr[i]);
+   }
+}
+
+printAnything<string>(['a', 'b', 'c']);
+printAnything(['d', 'e', 'f']);
+
+/* ========================================================================== */
+// Generic constraints
+/* ========================================================================== */
+class Car {
+   public print() {
+      console.log('I am a car');
+   }
+}
+
+class House {
+   public print() {
+      console.log('I am a house');
+   }
+}
+
+// this has the potential error
+function printHousesOrCars<T>(arr: Array<T>): void {
+   for (let i = 0; i < arr.length; i += 1) {
+      arr[i].print();
+   }
+}
+
+printHousesOrCars([1, 2, 3, 4]);
+
+// this uses the constraint to guarantee that the type we pass in will work
+interface Printable {
+   print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: Array<T>): void {
+   for (let i = 0; i < arr.length; i += 1) {
+      arr[i].print();
+   }
+}
+
+// does this T declaration work with this union type?
+printHousesOrCars<Car | House>([new Car(), new House()]);
