510f46b0ccc808fe64376ebefe6c0ade26e42cd8
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-04-02 19:42:32 -0700

    feat(main): add ".cache" dirs to git ignore and remove from tracking;

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,14 +1,13 @@
 /* ========================================================================== */
 // ALL REQUIRED IMPORTS
 /* ========================================================================== */
-
 // React
 // Packages
+import axios from 'axios';
 // Context / Store / Router
 // Components / Classes / Controllers / Services
 // Assets
 // Constants / Models / Interfaces / Types
-import { User } from './models/User';
 // Utils / Methods / Mocks
 // Styles
 
@@ -18,36 +17,7 @@ import { User } from './models/User';
 /* ========================================================================== */
 // DEFINING THE `INDEX` FILE
 /* ========================================================================== */
-console.debug('————————————————————————');
-const user: User = new User({ age: 100, name: 'Sloane' });
-console.debug(user.get('age'));
-console.debug(user.get('name'));
-
-console.debug('————————————————————————');
-user.set({ age: 200 });
-console.debug(user.get('age'));
-console.debug(user.get('name'));
-
-console.debug('————————————————————————');
-user.set({ name: 'Rooney' });
-console.debug(user.get('age'));
-console.debug(user.get('name'));
-
-console.debug('————————————————————————');
-user.on('change', () => {
-   console.debug('change #1');
-});
-user.on('change', () => {
-   console.debug('change #2');
-});
-user.on('bllr', () => {
-   console.debug('bllr was triggered');
-});
-user.trigger('change');
-user.trigger('bllr');
-user.trigger('disNoWorky');
-
-console.debug('————————————————————————');
+axios.post();
 
 /* ========================================================================== */
 // ALL REQUIRED EXPORTS
