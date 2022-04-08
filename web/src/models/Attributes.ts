ec295cf4012416be41e111c96c942b737b518feb
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-04-07 21:05:10 -0700

    feat(main): add `Attributes` class for `User`;

--- web/src/models/Attributes.ts
+++ web/src/models/Attributes.ts
@@ -5,8 +5,6 @@
 // Packages
 // Context / Store / Router
 // Components / Classes / Controllers / Services
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
 // Assets
 // Constants / Models / Interfaces / Types
 // Utils / Methods / Mocks
@@ -15,18 +13,19 @@ import { Sync } from './Sync';
 /* ========================================================================== */
 // INTERNAL HELPERS, INTERFACES, VARS & SET UP
 /* ========================================================================== */
-export interface UserProps {
-   age?: number;
-   id?: number;
-   name?: string;
-}
-
-const rootUrl = 'http://localhost:3000/users';
-
 /* ========================================================================== */
-// DEFINING THE `USER` CLASS
+// DEFINING THE `ATTRIBUTES` CLASS
 /* ========================================================================== */
-export class User {
-   public events: Eventing = new Eventing();
-   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+export class Attributes<T> {
+   constructor(private data: T): void {}
+
+   public get(keyOrIdx: string): string | number {
+      return this.data[keyOrIdx];
+   }
+
+   public set(newValues: T): void {
+      // NOTE **[G]** :: Which version is/(do I like) "better" for me?
+      Object.assign(this.data, newValues);
+      // this.data = { ...this.data, ...newValues };
+   }
 }
