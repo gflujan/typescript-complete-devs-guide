285a3269887d0060f58d5b6d9028cf1713893029
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-05-03 23:35:44 -0700

    feat(main): web proj -- add `views` directory and create basic `UserForm` class;

---
+++ web/views/UserForm.ts
@@ -0,0 +1,32 @@
+/* ========================================================================== */
+// ALL REQUIRED IMPORTS
+/* ========================================================================== */
+// React
+// Packages
+// Context / Store / Router
+// Components / Classes / Controllers / Services
+// Assets
+// Constants / Models / Interfaces / Types
+// Utils / Methods / Mocks
+// Styles
+
+/* ========================================================================== */
+// INTERNAL HELPERS, INTERFACES, VARS & SET UP
+/* ========================================================================== */
+/* ========================================================================== */
+// DEFINING THE `USER FORM` CLASS
+/* ========================================================================== */
+export class UserForm {
+   parent: Element;
+
+   constructor() {}
+
+   template(): string {
+      return `
+         <div class="user-form">
+            <h1>User Form</h1>
+            <input />
+         </div>
+      `;
+   }
+}
