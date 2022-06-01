6b6a5edb2df7d9541bd83737038a50cb2efb4eae
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-05-31 19:58:45 -0700

    feat(main): `server` proj -- set up `express` and middlewares; add type def packages for `express` and middlewares; create Express `app` and listen on port 3000;

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,37 @@
-console.debug('hola pendejo');
+/* ========================================================================== */
+// ALL REQUIRED IMPORTS
+/* ========================================================================== */
+// React
+// Packages
+import express from 'express';
+// Context / Store / Router
+// Components / Classes / Controllers / Services
+// Assets
+// Constants / Models / Interfaces / Types
+import { Request, Response } from 'express';
+// Utils / Methods / Mocks
+// Styles
+
+/* ========================================================================== */
+// INTERNAL HELPERS, INTERFACES, VARS & SET UP
+/* ========================================================================== */
+const app = express();
+
+/* ========================================================================== */
+// DEFINING THE `INDEX ENTRY POINT` FILE
+/* ========================================================================== */
+app.get('/', (req: Request, res: Response) => {
+   res.send(`
+      <div>
+         <h1>Hola Pendejo</h1>
+      </div>
+   `);
+});
+
+app.listen(3000, () => {
+   console.debug('Listening on port 3000');
+});
+
+/* ========================================================================== */
+// ALL REQUIRED EXPORTS
+/* ========================================================================== */
