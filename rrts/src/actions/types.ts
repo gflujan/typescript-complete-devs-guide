e347942ca7f22bc7abd908f0d76aa54ad60e9557
Gabriel M. Lujan <gfl.wrk@gmail.com>
2022-06-12 22:44:08 -0700

    feat(main): `react-redux` proj -- add `ActionTypes` enum and a type def for our Axios response;

--- rrts/src/actions/types.ts
+++ rrts/src/actions/types.ts
@@ -3,40 +3,22 @@
 /* ========================================================================== */
 // React
 // Packages
-import axios from 'axios';
-import { Dispatch } from 'redux';
 // Context / Stores / Routers
 // Components / Classes / Controllers / Services
 // Assets
 // Constants / Models / Interfaces / Types
-import { ActionTypes } from './types';
 // Utils / Methods / Mocks / Decorators
 // Styles
 
 /* ========================================================================== */
 // INTERNAL HELPERS, INTERFACES, VARS & SET UP
 /* ========================================================================== */
-const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
-
-interface Todo {
-   completed: boolean;
-   id: number;
-   title: string;
+/* ========================================================================== */
+// DEFINING THE `GLOBAL REDUX` ACTION TYPES
+/* ========================================================================== */
+export enum ActionTypes {
+   FetchTodos = 'FetchTodos',
 }
-
-/* ========================================================================== */
-// DEFINING THE `GLOBAL REDUX` ACTIONS
-/* ========================================================================== */
-export const fetchTodos = () => {
-   return async (dispatch: Dispatch): void => {
-      const response = await axios.get<Todo[]>(TODOS_URL);
-
-      dispatch({
-         type: ActionTypes.FetchTodos,
-         payload: response.data,
-      });
-   };
-};
 
 /* ========================================================================== */
 // ALL REQUIRED EXPORTS
